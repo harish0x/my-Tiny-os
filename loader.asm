b3594bee7484e7a391384fbb503893c6ec0f5c03
harish0x <harishankar.amk@gmail.com>
2024-03-22 23:13:35 +0530

    Boot-loader

---
+++ loader.asm
@@ -0,0 +1,23 @@
+bits 16
+org 0x7c00
+
+mov si,hello
+mov ah,0x0e
+
+
+loop:
+    lodsb
+    or al,al
+    jz halt
+    int 0x10
+
+    jmp loop
+
+halt:
+    cli
+    hlt 
+hello:
+    db "Itzz, working",0
+
+times 510-($-$$) db 0
+dw 0xaa55
